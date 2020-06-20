d7a5241d28d0c004fda5f01926591207cbe60cf7
Bob Maerten <bob.maerten@gmail.com>
2020-06-20 14:18:35 +0200

    Add numericality validator to null constraint checker (#57)

--- spec/checkers/null_constraint_checker_spec.rb
+++ spec/checkers/null_constraint_checker_spec.rb
@@ -8,7 +8,10 @@ RSpec.describe DatabaseConsistency::Checkers::NullConstraintChecker do
 
   test_each_database do
     before do
-      define_database_with_entity { |table| table.string :email, null: false }
+      define_database_with_entity do |table|
+        table.string :email, null: false
+        table.integer :count, null: false
+      end
     end
 
     context 'when validation is missing' do
@@ -33,6 +36,36 @@ RSpec.describe DatabaseConsistency::Checkers::NullConstraintChecker do
           checker_name: 'NullConstraintChecker',
           table_or_model_name: klass.name,
           column_or_attribute_name: 'email',
+          status: :ok,
+          message: nil
+        )
+      end
+    end
+
+    context 'when has numericality validation with allow_nil' do
+      let(:klass) { define_class { |klass| klass.validates_numericality_of :count, allow_nil: true } }
+      subject(:checker) { described_class.new(model, klass.columns.last) }
+
+      specify do
+        expect(checker.report).to have_attributes(
+          checker_name: 'NullConstraintChecker',
+          table_or_model_name: klass.name,
+          column_or_attribute_name: 'count',
+          status: :fail,
+          message: 'column is required in the database but do not have presence validator'
+        )
+      end
+    end
+
+    context 'when has numericality validation without allow_nil' do
+      let(:klass) { define_class { |klass| klass.validates_numericality_of :count } }
+      subject(:checker) { described_class.new(model, klass.columns.last) }
+
+      specify do
+        expect(checker.report).to have_attributes(
+          checker_name: 'NullConstraintChecker',
+          table_or_model_name: klass.name,
+          column_or_attribute_name: 'count',
           status: :ok,
           message: nil
         )
