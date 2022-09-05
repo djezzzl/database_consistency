b1006e4947d8175e0e0facebfa85e60c181ceeb3
soloveytani <soloveystn@gmail.com>
2022-09-05 21:14:10 +0200

    Add TODO generation

---
+++ lib/database_consistency/writers/todo_writer.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module DatabaseConsistency
+  # The module contains formatters
+  module Writers
+    # The writer that generates to-do file
+    class TodoWriter < BaseWriter
+      def write
+        hash = results.each_with_object({}) do |result, hash|
+          next unless write?(result.status)
+
+          assign_result(hash, result)
+        end
+
+        File.write(file_name, hash.to_yaml)
+      end
+
+      def write?(status)
+        status == :fail
+      end
+
+      private
+
+      def assign_result(hash, result)
+        hash[result.table_or_model_name] ||= {}
+        hash[result.table_or_model_name][result.column_or_attribute_name] ||= {}
+        hash[result.table_or_model_name][result.column_or_attribute_name][result.checker_name] = {'enabled' => false}
+      end
+
+      def file_name
+        [nil, *(1..100)].each do |number|
+          name = generate_file_name(number)
+
+          return name unless File.exists?(name)
+        end
+      end
+
+      def generate_file_name(number = nil)
+        ".database_consistency.todo#{number}.yml"
+      end
+    end
+  end
+end
