93b645e62e2246f217b6ef017a0a458ee33c34ae
djezzzl <lawliet.djez@gmail.com>
2022-10-31 08:00:32 +0300

    Fixes

--- lib/database_consistency/writers/todo_writer.rb
+++ lib/database_consistency/writers/todo_writer.rb
@@ -6,13 +6,13 @@ module DatabaseConsistency
     # The writer that generates to-do file
     class TodoWriter < BaseWriter
       def write
-        hash = results.each_with_object({}) do |result, hash|
+        h = results.each_with_object({}) do |result, hash|
           next unless write?(result.status)
 
           assign_result(hash, result)
         end
 
-        File.write(file_name, hash.to_yaml)
+        File.write(file_name, h.to_yaml)
       end
 
       def write?(status)
@@ -24,14 +24,14 @@ module DatabaseConsistency
       def assign_result(hash, result)
         hash[result.table_or_model_name] ||= {}
         hash[result.table_or_model_name][result.column_or_attribute_name] ||= {}
-        hash[result.table_or_model_name][result.column_or_attribute_name][result.checker_name] = {'enabled' => false}
+        hash[result.table_or_model_name][result.column_or_attribute_name][result.checker_name] = { 'enabled' => false }
       end
 
       def file_name
         [nil, *(1..100)].each do |number|
           name = generate_file_name(number)
 
-          return name unless File.exists?(name)
+          return name unless File.exist?(name)
         end
       end
 
