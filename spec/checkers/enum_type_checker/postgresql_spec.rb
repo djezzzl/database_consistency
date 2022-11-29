9bb176d13757b511387f6160b186dd270be85b9f
Evgeniy Demin <lawliet.djez@gmail.com>
2022-11-29 13:32:17 +0300

    Fix EnumTypeChecker (#161)

---
+++ spec/checkers/enum_type_checker/postgresql_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+RSpec.describe DatabaseConsistency::Checkers::EnumTypeChecker, :postgresql do
+  subject(:checker) { described_class.new(model, enum) }
+
+  before do
+    unless ActiveRecord::VERSION::MAJOR >= 7 && adapter == 'postgresql'
+      skip('older versions are not supported with sqlite3')
+    end
+  end
+
+  let(:model) { entity_class }
+  let(:enum) { entity_class.defined_enums.keys.first }
+  let!(:entity_class) do
+    define_class do |klass|
+      klass.enum field: { value1: 'valueu1', value2: 'value2' }
+    end
+  end
+
+  context 'with enum type' do
+    before do
+      define_database do
+        create_enum :field_type, %w[value1 value2]
+
+        create_table :entities do |t|
+          t.enum :field, enum_type: 'field_type'
+        end
+      end
+    end
+
+    specify do
+      expect(checker.report).to have_attributes(
+        checker_name: 'EnumTypeChecker',
+        table_or_model_name: entity_class.name,
+        column_or_attribute_name: 'field',
+        status: :ok,
+        error_message: nil,
+        error_slug: nil
+      )
+    end
+  end
+end
