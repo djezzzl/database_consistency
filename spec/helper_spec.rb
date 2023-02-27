cf267a59e5c1f9e1e51cb6f6e99d7bfd09bd199f
Bartek Bułat <bartek.bulat@gmail.com>
2023-02-27 16:21:10 +0100

    Fix parent_models method to return all parent models for the table
    Until now, parent_models method was returning only one model from the
    root of the inheretance tree, but it might be a case some tables have 2
    different top-level classes defined (e.g. fori different
    contexts/engines/etc.). The class that was returned from
    `min_by` was actually determined by load order (an order of
    `descendants` method which actually changes definition between Ruby 3.0
    and 3.3 on Rails 7)

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe DatabaseConsistency::Helper, :sqlite, :mysql, :postgresql do
-  subject { described_class.first_level_associations(child) }
-
   describe '#first_level_associations' do
+    subject { described_class.first_level_associations(child) }
+
     let(:parent) { Class.new(ActiveRecord::Base) { |klass| klass.has_one :user } }
 
     context 'when only parent defines association' do
@@ -14,6 +14,25 @@ RSpec.describe DatabaseConsistency::Helper, :sqlite, :mysql, :postgresql do
     context 'when child redefines association' do
       let(:child) { Class.new(parent) { |klass| klass.has_one :user } }
       it { expect(subject.size).to eq(1) }
+    end
+  end
+
+  describe '#parent_models' do
+    subject(:parent_models) { described_class.parent_models }
+
+    before do
+      allow(described_class).to receive(:project_klass?).and_return(true)
+
+      define_database_with_entity { |table| table.string :email }
+
+      define_class('Entities', :entities)
+      define_class('Scoped::Entities', :entities)
+      stub_const('SubEntities', Class.new(Entities))
+    end
+
+    it 'includes top-level classes only' do
+      expect(subject).to include(Entities, Scoped::Entities)
+      expect(subject).not_to include(SubEntities)
     end
   end
 end
