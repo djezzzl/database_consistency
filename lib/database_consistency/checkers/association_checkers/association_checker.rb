6b08947830bada0351f556a798d76d30a99e1db2
Patrick McSweeny <patmcsweeny@gmail.com>
2025-11-24 11:26:20 -0500

    Add checker to find missing dependent destroy associations

--- lib/database_consistency/checkers/association_checkers/association_checker.rb
+++ lib/database_consistency/checkers/association_checkers/association_checker.rb
@@ -23,6 +23,13 @@ module DatabaseConsistency
       def table_or_model_name
         @table_or_model_name ||= model.name.to_s
       end
+
+      def foreign_key_exists? # rubocop:disable Metrics/AbcSize
+        model.connection.foreign_keys(model.table_name).any? do |foreign_key|
+          (Helper.extract_columns(association.foreign_key.to_s) - Array.wrap(foreign_key.column)).empty? &&
+            foreign_key.to_table == association.klass.table_name
+        end
+      end
     end
   end
 end
