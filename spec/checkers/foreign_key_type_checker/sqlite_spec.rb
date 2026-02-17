7932851c563427b68930f04f3f376b90572c2707
Evgenii Demin <lawliet.djez@gmail.com>
2026-02-17 10:52:16 +0100

    Fix conditions

--- spec/checkers/foreign_key_type_checker/sqlite_spec.rb
+++ spec/checkers/foreign_key_type_checker/sqlite_spec.rb
@@ -343,8 +343,10 @@ RSpec.describe DatabaseConsistency::Checkers::ForeignKeyTypeChecker, :sqlite do
     context 'when foreign key has matching composite keys' do
       let!(:user_class) { define_class('User', :users) { |klass| klass.primary_key = %i[id account_id] } }
       let!(:company_class) do
+        key = ActiveRecord::VERSION::MAJOR == 7 && ActiveRecord::VERSION::MINOR == 1 ? :query_constraints : :foreign_key
+
         define_class('Company', :companies) do |klass|
-          klass.belongs_to :user, query_constraints: %i[user_id account_id]
+          klass.belongs_to :user, key => %i[user_id account_id]
         end
       end
 
